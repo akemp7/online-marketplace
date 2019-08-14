12e5e211c3fbcd48a42dd9c367856da358f3ded3
Anita Kemp and Rachael Bouissey <rachael@>
2019-08-14 11:46:47 -0700

    Add order details and receipt

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,33 @@
-function add (number1, number2) {
- return number1 + number2
-};
-
-var subtract = function (number1, number2) {
- return number1 - number2
-} ;
-
-var multiply = function (number1, number2) {
- return number1 * number2
-} ;
-
-var divide = function (number1, number2) {
- return number1 / number2
-} ;
+// function add (number1, number2) {
+//  return number1 + number2
+// };
+//
+// var subtract = function (number1, number2) {
+//  return number1 - number2
+// } ;
+//
+// var multiply = function (number1, number2) {
+//  return number1 * number2
+// } ;
+//
+// var divide = function (number1, number2) {
+//  return number1 / number2
+// } ;
 
 
 $(document).ready(function(){
- $("form#add").submit(function(event){
+ $(".formOne").submit(function(event){
 
- event.preventDefault();
- var number1 = parseInt($("#add1").val());
- var number2 = parseInt($("#add2").val());
- var result = multiply(number1, number2);
- $("#output").text(result);
+   var styleInput =  $("select#Style").val();
+   var nameInput = $("input#name").val();
+   var addressInput = $("input#address").val();
+ // var number1 = parseInt($("#add1").val());
+ // var number2 = parseInt($("#add2").val());
+ // var result = multiply(number1, number2);
+ $(".output").text(styleInput + " " + nameInput + " " + addressInput);
+
+event.preventDefault();
+
 
 });
 });
